b8b1a47c459cb72f12dbb04d67eaea684cc5538f
zZLerisZz <yalerisatinet@gmail.com>
2024-11-13 15:45:46 +0300

    Simple change in scenes

--- Scenes/Scripts/main.gd
+++ Scenes/Scripts/main.gd
@@ -21,8 +21,12 @@ func _on_back_to_menu_button_pressed() -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/world.tscn")
-
+	var world_scene = load("res://Scenes/world.tscn").instantiate()
+	world_scene.ip_address = "1111"
+	#get_tree().change_scene_to_file("res://Scenes/world.tscn")
+	get_tree().current_scene.queue_free()
+	get_tree().root.add_child(world_scene)
+	get_tree().current_scene = world_scene
 
 func _on_create_game_lobby_button_pressed() -> void:
 	$GameMenuInterface.visible = false
--- Scenes/Scripts/world.gd
+++ Scenes/Scripts/world.gd
@@ -1,5 +1,10 @@
 extends Node
 
+@export var ip_address: String
+
+func _ready() -> void:
+	print_debug(ip_address)
+
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
