7c003f7cb605d2351243484088d2693dfe9c639d
zZLerisZz <yalerisatinet@gmail.com>
2024-11-13 14:50:13 +0300

    Player movement

--- Scenes/Scripts/player.gd
+++ Scenes/Scripts/player.gd
@@ -1,22 +1,35 @@
 extends CharacterBody3D
 
+@onready var camera = $Camera3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 10.0
+const JUMP_VELOCITY = 10
 
+var sensivity = 0.002
+
+var gravity = 20.0
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * sensivity)
+		camera.rotate_x(-event.relative.y * sensivity)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
---
+++ Scenes/Scripts/world.gd
@@ -0,0 +1,5 @@
+extends Node
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
