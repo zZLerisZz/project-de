dc6796f3a522b00ad7cfaffef054f2a65b706258
zZLerisZz <yalerisatinet@gmail.com>
2024-11-13 18:11:42 +0300

    Menus interfaces

--- Scenes/Scripts/main.gd
+++ Scenes/Scripts/main.gd
@@ -1,9 +1,24 @@
 extends Node
 
 func _ready() -> void:
+	$MainMenuInterface.visible = true
 	$GameMenuInterface.visible = false
 	$SettingsMenuInterface.visible = false
-	$JoinCreateMenu.visible = false
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("quit"):
+		if $MainMenuInterface.visible:
+			get_tree().quit()
+		
+		if $GameMenuInterface.visible:
+			if $GameMenuInterface/ChooseMenu.visible:
+				$GameMenuInterface.visible = false
+				$MainMenuInterface.visible = true
+			
+			if $GameMenuInterface/JoinCreateMenu.visible:
+				$GameMenuInterface/JoinCreateMenu.visible = false
+				$GameMenuInterface/ChooseMenu.visible = true
 
 
 func _on_exit_button_pressed() -> void:
@@ -12,7 +27,10 @@ func _on_exit_button_pressed() -> void:
 
 func _on_game_button_pressed() -> void:
 	$MainMenuInterface.visible = false
+	
 	$GameMenuInterface.visible = true
+	$GameMenuInterface/ChooseMenu.visible = true
+	$GameMenuInterface/JoinCreateMenu.visible = false
 
 
 func _on_back_to_menu_button_pressed() -> void:
@@ -20,14 +38,25 @@ func _on_back_to_menu_button_pressed() -> void:
 	$GameMenuInterface.visible = false
 
 
-func _on_button_pressed() -> void:
+func _on_create_game_lobby_button_pressed() -> void:
+	$GameMenuInterface/ChooseMenu.visible = false
+	
+	$GameMenuInterface/JoinCreateMenu.visible = true
+	$GameMenuInterface/JoinCreateMenu/CreateMenu.visible = true
+	$GameMenuInterface/JoinCreateMenu/JoinMenu.visible = false
+
+
+func _on_create_server_button_pressed() -> void:
 	var world_scene = load("res://Scenes/world.tscn").instantiate()
-	world_scene.ip_address = "1111"
-	#get_tree().change_scene_to_file("res://Scenes/world.tscn")
+	world_scene.ip_address = $GameMenuInterface/JoinCreateMenu/CreateMenu/MarginContainer/VBoxContainer/IPAdressLineEdit.text
 	get_tree().current_scene.queue_free()
 	get_tree().root.add_child(world_scene)
 	get_tree().current_scene = world_scene
 
-func _on_create_game_lobby_button_pressed() -> void:
-	$GameMenuInterface.visible = false
-	$JoinCreateMenu.visible = true
+
+func _on_join_game_lobby_button_pressed() -> void:
+	$GameMenuInterface/ChooseMenu.visible = false
+	
+	$GameMenuInterface/JoinCreateMenu.visible = true
+	$GameMenuInterface/JoinCreateMenu/CreateMenu.visible = false
+	$GameMenuInterface/JoinCreateMenu/JoinMenu.visible = true
